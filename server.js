b951ecc24d87de35a6bddd0bbaf4488fe57c35cc
Audra Gault <jgault87@gmail.com>
2022-05-13 13:29:17 -0600

    added initial inquirer prompt and handoff functions from switchcase stmt

--- server.js
+++ server.js
@@ -9,5 +9,72 @@ const db = mysql.createConnection(
         password: 'bootcampsql',
         database: 'employees_db'
     },
-    console.log(`Connected to employee database`)
+    console.log('Connected to employee database')
 );
+
+
+const openPrompt = () => {
+    inquirer.prompt([
+        {
+          type: "list",
+          message: "Please select an option",
+          name: "option",
+          choices: [
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add a Department",
+            "Add a Role",
+            "Update an Employee Role",
+            "Exit",
+          ],
+        },
+      ])
+      //switch case for userselection, if user chooses not to add further it will handoff to generateHTML function
+      .then(function (data) {
+        switch (data.option) {
+          case "View All Departments":
+            deptView();
+            break;
+          case "View All Roles":
+            roleView();
+            break;
+          case "View All Employees":
+            empView();
+            break;
+          case "Add a Department":
+            deptAdd();
+            break;
+          case "Add a Role":
+            roleAdd();
+            break;
+          case "Update an Employee Role":
+            updateRole();
+            break;
+  
+          default:
+            exit();
+        }
+      });
+  }
+  
+
+
+const deptView = () => {}
+const roleView = () => {}
+const empView = () => {}
+const deptAdd = () => {}
+const roleAdd = () => {}
+const updateRole = () => {}
+
+
+
+
+
+
+
+  function init() {
+      openPrompt();
+  }
+
+  init();
